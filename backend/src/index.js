e4ebdd83cbd332d67d58ac00b154fb865010d98f
yashpratap09 <yashraja9630@gmail.com>
2023-02-01 22:17:33 +0530

    last

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const route = require('../src/route/route');
 const app = express()
+require("dotenv").config()
 
 
 const cors = require('cors')
@@ -22,5 +23,7 @@ app.use(cors())
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3001, function () {
-	console.log('Express app running on port ' + (process.env.PORT || 3001))})
+
+
+app.listen(process.env.MONGODB_URL || 3001, function () {
+	console.log('Express app running on port ' + (process.env.MONGODB_URL || 3001))})
