208b30523b1edb21e6997c63263e1e3db7340194
yashpratap09 <yashraja9630@gmail.com>
2023-02-13 16:54:05 +0530

    by yash

--- reactassignment/src/insta.js
+++ reactassignment/src/insta.js
@@ -24,7 +24,7 @@ const Insta = () => {
 
 
       axios
-        .post("http://localhost:3001/createProject", {
+        .post("https://instagram-aphv.onrender.com/createProject", {
           Name, Password
 
         }).then((response) => {
